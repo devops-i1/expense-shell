8e881a719d594f8f672db7508b5e1ed01db394b6
Akhil <akhilmahendra035@gmail.com>
2024-03-09 13:51:34 +0530

    updating backend script with  mysql server ip

--- backend.sh
+++ backend.sh
@@ -17,4 +17,4 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.92.149 -uroot -pExpenseApp@1 < /app/schema/backend.sql
