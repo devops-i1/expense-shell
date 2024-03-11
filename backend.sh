609b1c9377e9b6828c845403cc3dbdaaeda9ff21
Akhil <akhilmahendra035@gmail.com>
2024-03-11 17:55:24 +0530

    moving the heading to function and also adding to log file.

--- backend.sh
+++ backend.sh
@@ -1,57 +1,63 @@
 mysql_root_password=$1
 
-echo Disable defult Nodejs version module
+Print_Task_Heading () {
+  echo $1
+  echo "#############" $1 "############"  &>>/tmp/expense.log
+}
+
+
+Print_Task_Heading "Disable defult Nodejs version module"
 dnf module disable nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo Enable Nodejs module for v20
-dnf module enable nodejs:20 -y &>>/tmp/expense.log
+Print_Task_Heading "Enable Nodejs module for v20"
+dnf module enable nodejs:20 -y C
 echo $?
 
-echo Install Nodejs
+Print_Task_Heading "Install Nodejs"
 dnf install nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo Adding application user
+Print_Task_Heading "Adding application user"
 useradd expense &>>/tmp/expense.log
 echo $?
 
-echo Copy backend service file
+Print_Task_Heading "Copy backend service file"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 echo $?
 
-echo Clean the old content
+Print_Task_Heading "Clean the old content"
 rm -rf /app &>>/tmp/expense.log
 echo $?
 
-echo Craete app directory
+Print_Task_Heading "Craete app directory"
 mkdir /app &>>/tmp/expense.log
 echo $?
 
-echo Download app content
+Print_Task_Heading "Download app content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
 
-echo Extract app content
+Print_Task_Heading "Extract app content"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
 
-echo Download Nodejs dependencies
+Print_Task_Heading "Download Nodejs dependencies"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
 echo $?
 
-echo Start Backend service
+Print_Task_Heading "Start Backend service"
 systemctl daemon-reload &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
 echo $?
 
-echo Install MySQL client
+Print_Task_Heading "Install MySQL client"
 dnf install mysql -y &>>/tmp/expense.log
 echo $?
 
-echo Load schema
+Print_Task_Heading "Load schema"
 mysql -h 172.31.10.122 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
 echo $?
