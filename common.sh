7fc37daebfce9dcd61d9f2c8a915f8f0c4d35c8c
Akhil <akhilmahendra035@gmail.com>
2024-03-12 15:56:04 +0530

    adding color status to status check function

--- common.sh
+++ common.sh
@@ -6,8 +6,8 @@ Print_Task_Heading () {
 
 Check_status() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31mSUCCESS\e[0m"
+    echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo -e "\e[32mFAILURE\e[0m"
+    echo -e "\e[31mFAILURE\e[0m"
   fi
 }
